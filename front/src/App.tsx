9d198baa444a3ec686f5dbf2e65e858b325da01e
mr.tsowa <tsowa48@gmail.com>
2023-10-29 00:25:07 +0300

    init navbar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './styles/bootstrap.css';
 import Main from './pages/Main'
+import {NavBar} from './components/NavBar'
 
 function App() {
-  return (
-    <div className="App container">
-        <br/>
-      <Main/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavBar/>
+            <Main/>
+            <footer>
+
+            </footer>
+        </div>
+    );
 }
 
 export default App;
--- front/src/pages/Main.tsx
+++ front/src/pages/Main.tsx
@@ -5,27 +5,34 @@ export default class Main extends Component<any, any> {
 
     render() {
         return (
-            <div className="container">
-                <div className="row mb-5">
-                    {Array.from({ length: 15 }).map(() =>
-                        <News newsId={3} title='Akulaplay - магазин с душой.'
-                              image='https://media.cntraveller.com/photos/611bf0b8f6bd8f17556db5e4/master/pass/gettyimages-1146431497.jpg'
-                              authorId={1}
-                              authorName='Иванов И.'
-                              authorAvatar='https://www.vhv.rs/dpng/d/551-5511364_circle-profile-man-hd-png-download.png'
-                              date='15 октября 2023'>
-                            Мы - команда, которая профессионально улучшает Ваше впечатление от собственного авто!
-                            Более 5 лет мы помогаем нашим клиентам сделать правильный выборр и подбираем для их авто
-                            автомагнитолы на андроид, акустические системы и автоэлектронику.
-                            Наши клиенты часто говорят "Ваа-у!" после преображения их авто! В 100% случаев изменения
-                            заключаются в правильном подборе устройств, переходников и корректной установке.
-                            Скажи "Ваа-у!" почувствовав новые грани своего авто!
-                            Напиши по ссылке: https://vk.me/akulaopt_86
-                            И узнай, что мы можем тебе предложить!
-                        </News>
-                    )}
+            <main>
+                <div className={'section'}>
+                    <div className="container">
+                        <div className="row mb-5">
+                            {Array.from({length: 15}).map(() =>
+                                <News newsId={3} title='Akulaplay - магазин с душой.'
+                                      image='https://media.cntraveller.com/photos/611bf0b8f6bd8f17556db5e4/master/pass/gettyimages-1146431497.jpg'
+                                      authorId={1}
+                                      authorName='Иванов И.'
+                                      authorAvatar='https://www.vhv.rs/dpng/d/551-5511364_circle-profile-man-hd-png-download.png'
+                                      date='15 октября 2023'>
+                                    Мы - команда, которая профессионально улучшает Ваше впечатление от собственного
+                                    авто!
+                                    Более 5 лет мы помогаем нашим клиентам сделать правильный выборр и подбираем для их
+                                    авто
+                                    автомагнитолы на андроид, акустические системы и автоэлектронику.
+                                    Наши клиенты часто говорят "Ваа-у!" после преображения их авто! В 100% случаев
+                                    изменения
+                                    заключаются в правильном подборе устройств, переходников и корректной установке.
+                                    Скажи "Ваа-у!" почувствовав новые грани своего авто!
+                                    Напиши по ссылке: https://vk.me/akulaopt_86
+                                    И узнай, что мы можем тебе предложить!
+                                </News>
+                            )}
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </main>
         )
     }
 }
